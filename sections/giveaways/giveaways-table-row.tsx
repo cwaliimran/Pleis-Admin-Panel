21b7209681b21b1ae34f69f1d9ae9f87f7081ac3
Suheer Zahid <glowingemp124@glowingsoft.com>
2025-12-10 11:46:25 +0500

    Client feedback about giveaway module done

--- sections/giveaways/giveaways-table-row.tsx
+++ sections/giveaways/giveaways-table-row.tsx
@@ -3,11 +3,26 @@
 import CustomBadge from '@/components/ui/custom-badge';
 import { TableCell, TableRow } from '@/components/ui/table';
 import { fDate, formatStr } from '@/utils/format-time';
-import { Pencil, Trash2 } from 'lucide-react';
+import { List, Pencil, Trash2 } from 'lucide-react';
 import { FC } from 'react';
 import { TableRowProps } from './types';
 
-const GiveawaysTableRow: FC<TableRowProps> = ({ item, handleDelete, handleEdit }) => {
+const GiveawaysTableRow: FC<TableRowProps> = ({ item, handleDelete, handleEdit, handleOpenWinners }) => {
+  const getGiveawayStatusVariant = (status: string) => {
+    switch (status) {
+      case 'upcoming':
+        return 'info';
+      case 'live':
+        return 'success';
+      case 'ended':
+        return 'error';
+      case 'completed':
+        return 'default';
+      default:
+        return 'info';
+    }
+  };
+
   return (
     <TableRow className="h-14 w-full transition-colors hover:bg-[#f5f5f5] dark:hover:bg-[#272727]/50">
       <TableCell className="text-left capitalize">{item?.title || '-'}</TableCell>
@@ -28,8 +43,25 @@ const GiveawaysTableRow: FC<TableRowProps> = ({ item, handleDelete, handleEdit }
         <CustomBadge variant={item.status === 'active' ? 'success' : item.status === 'inactive' ? 'error' : 'info'}>{item.status}</CustomBadge>
       </TableCell>
 
+      <TableCell className="text-left">
+        <CustomBadge variant={getGiveawayStatusVariant(item.giveawayStatus)}>{item.giveawayStatus}</CustomBadge>
+      </TableCell>
+
       <TableCell className="text-end">
         <div className="flex gap-2">
+          <button
+            title="View Details"
+            type="button"
+            disabled={item.giveawayStatus === 'live' || item.giveawayStatus === 'upcoming'}
+            onClick={(e) => {
+              e.stopPropagation();
+              handleOpenWinners?.(item?._id);
+            }}
+            className="cursor-pointer rounded-md bg-gray-100 p-1.5 transition hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-gray-800 dark:hover:bg-gray-700"
+          >
+            <List className="h-4 w-4 text-gray-700 dark:text-gray-200" />
+          </button>
+
           <button
             title="Edit Promo Code"
             type="button"
