32fd4eecfb501505e0fd86d234ab10822cdfcd27
Suheer Zahid <glowingemp124@glowingsoft.com>
2026-02-04 16:01:25 +0500

    QR Code Module - Organizer Side

--- store/Reducer/qrcodes-api.tsx
+++ store/Reducer/qrcodes-api.tsx
@@ -1,28 +1,32 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import API_ROUTES from '../apiRoutes';
-import { customFetchBaseQuery } from '../customFetchBaseQuery';
+import { customFetchBaseQueryWithRoleRouting } from '../utils/customFetchBaseQueryWithRoleRouting';
 
 export const qrcodesApi = createApi({
   reducerPath: 'qrcodesApi',
-  baseQuery: customFetchBaseQuery(),
+  baseQuery: customFetchBaseQueryWithRoleRouting(),
   tagTypes: ['qrcode'],
 
   endpoints: (builder) => ({
     getQrcodes: builder.query({
       query: ({ companyOrganizer }) => {
         const params: any = {
-          // keyword: search,
-          // status,
-          // page: page + 1,
           limit: 1000,
         };
 
-        // if (date) (params as any).date = date;
-        if (companyOrganizer) (params as any).companyOrganizer = companyOrganizer;
+        if (companyOrganizer) {
+          params.companyOrganizer = companyOrganizer;
+        }
+
         return {
-          url: API_ROUTES.ADMIN_QR_CODE,
+          url: '',
           method: 'GET',
           params,
+          roleBasedRouting: {
+            adminRoute: API_ROUTES.ADMIN_QR_CODE,
+            organizerRoute: API_ROUTES.ORGANIZER_QR_CODE,
+            // adminOnlyParams: ['companyOrganizer'], // enable if organizer must NOT send it
+          },
         };
       },
       transformResponse: (res) => ({
@@ -34,17 +38,25 @@ export const qrcodesApi = createApi({
 
     addQrcode: builder.mutation({
       query: (newQrcode) => ({
-        url: API_ROUTES.ADMIN_QR_CODE,
+        url: '',
         method: 'POST',
         body: newQrcode,
+        roleBasedRouting: {
+          adminRoute: API_ROUTES.ADMIN_QR_CODE,
+          organizerRoute: API_ROUTES.ORGANIZER_QR_CODE,
+        },
       }),
       invalidatesTags: ['qrcode'],
     }),
 
     deleteQrcode: builder.mutation({
       query: (id) => ({
-        url: API_ROUTES.ADMIN_QR_CODE_BY_ID(id),
+        url: '',
         method: 'DELETE',
+        roleBasedRouting: {
+          adminRoute: API_ROUTES.ADMIN_QR_CODE_BY_ID(id),
+          organizerRoute: API_ROUTES.ORGANIZER_QR_CODE_BY_ID(id),
+        },
       }),
       invalidatesTags: ['qrcode'],
     }),
@@ -52,3 +64,58 @@ export const qrcodesApi = createApi({
 });
 
 export const { useGetQrcodesQuery, useAddQrcodeMutation, useDeleteQrcodeMutation } = qrcodesApi;
+
+// import { createApi } from '@reduxjs/toolkit/query/react';
+// import API_ROUTES from '../apiRoutes';
+// import { customFetchBaseQuery } from '../customFetchBaseQuery';
+
+// export const qrcodesApi = createApi({
+//   reducerPath: 'qrcodesApi',
+//   baseQuery: customFetchBaseQuery(),
+//   tagTypes: ['qrcode'],
+
+//   endpoints: (builder) => ({
+//     getQrcodes: builder.query({
+//       query: ({ companyOrganizer }) => {
+//         const params: any = {
+//           // keyword: search,
+//           // status,
+//           // page: page + 1,
+//           limit: 1000,
+//         };
+
+//         // if (date) (params as any).date = date;
+//         if (companyOrganizer) (params as any).companyOrganizer = companyOrganizer;
+//         return {
+//           url: API_ROUTES.ADMIN_QR_CODE,
+//           method: 'GET',
+//           params,
+//         };
+//       },
+//       transformResponse: (res) => ({
+//         data: res.data,
+//         meta: res.meta,
+//       }),
+//       providesTags: ['qrcode'],
+//     }),
+
+//     addQrcode: builder.mutation({
+//       query: (newQrcode) => ({
+//         url: API_ROUTES.ADMIN_QR_CODE,
+//         method: 'POST',
+//         body: newQrcode,
+//       }),
+//       invalidatesTags: ['qrcode'],
+//     }),
+
+//     deleteQrcode: builder.mutation({
+//       query: (id) => ({
+//         url: API_ROUTES.ADMIN_QR_CODE_BY_ID(id),
+//         method: 'DELETE',
+//       }),
+//       invalidatesTags: ['qrcode'],
+//     }),
+//   }),
+// });
+
+// export const { useGetQrcodesQuery, useAddQrcodeMutation, useDeleteQrcodeMutation } = qrcodesApi;
